9aea66be6c51698620360df592a2343d2aa23a9c
priyanshasharma24 <138128911+priyanshasharma24@users.noreply.github.com>
2024-02-08 03:38:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -114,7 +114,7 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="pro" onClick={() => window.open('https://dalle07804.onrender.com/', '_blank')}>
+            <div className="pro" onClick={() => window.open('https://dalle-e-07804.vercel.app/', '_blank')}>
               <img
                 src="https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Fdalle-clone.webp&w=750&q=75"
                 alt=""
@@ -217,7 +217,7 @@ function App() {
                 </div>
               </a>
 
-              <a href="/" className="a">
+              <a href="https://iv-download07804.netlify.app/" className="a">
               <svg viewBox="0 0 24 24" fill="none" width="24" height="24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#ffffff" stroke-width="1.6320000000000001" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 17H12H8" stroke="#fff" stroke-width="1.6320000000000001" stroke-linecap="round"></path> <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="#fff" stroke-width="1.6320000000000001"></path> </g></svg>                  <div className="col">
                 <span>
                 SnapInsta Clone
